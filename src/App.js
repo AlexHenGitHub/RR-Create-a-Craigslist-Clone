b6f840306847bf617da39c0756472e2a396d55e5
AlexHenGitHub <108377905+AlexHenGitHub@users.noreply.github.com>
2022-12-01 23:03:57 -0500

    SearchBar.jsx

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 
 // Import components
 import './App.css';
+import SearchBar from './SearchBar.jsx'
 
 function App(){
   return (
     <div>
       <h1>craigslist</h1>
       <div className="App">
-        {/* Your content will go here! */}
+        <SearchBar />
       </div>
     </div>
   );
